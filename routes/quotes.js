ca70e8250c67597f076b4ca6807ba8b12e5159bd
sakshiagrawal <jsaloni1104@gmail.com>
2021-04-18 13:49:07 +0530

    mongo project uploaded

---
+++ routes/quotes.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+// const router = express.Router();
+// // const studentSchema = require("../models/quotes");
+
+// router.post("/add", function(req, res){
+//      res.send({"result": "success"});
+// });
+
+
+
+// module.exports = router;
